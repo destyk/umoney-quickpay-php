21a35fa0240131e2d8ca50f288621631ee013822
destyk <nikita.karpov.1910@mail.ru>
2022-07-18 14:53:59 +0800

    Added Exception.php

---
+++ src/Exception.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * ЮMoney quickpay SDK.
+ *
+ * @package   destyk/umoney-quickpay-php
+ * @author    Nikita Karpov <nikita.karpov.1910@mail.ru>
+ * @copyright 2022 (c) DestyK
+ * @license   MIT https://raw.githubusercontent.com/destyk/umoney-quickpay-php/main/LICENSE
+ */
+
+namespace DestyK\UMoney;
+
+use ErrorException;
+
+/**
+ * General Exception Handling
+ */
+class Exception extends ErrorException
+{
+}
